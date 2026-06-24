4261b71aa3aa3940c3ce85b94bd12c4fab4d3637
Bradley <72202351+BradleyMaxwell@users.noreply.github.com>
2026-06-24 18:37:37 +0100

    card view prefab

---
+++ Assets/Scripts/Cards/CardView.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private TextMeshProUGUI manaCostText;
+}
