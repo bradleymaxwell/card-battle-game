c010016c1d40df5c28d8cdbf5d9f7a6d950b0164
Bradley <72202351+BradleyMaxwell@users.noreply.github.com>
2026-06-27 21:11:21 +0100

    highlighting action views when hovered and performed

--- Assets/Scripts/Units/Actions/ActionView.cs
+++ Assets/Scripts/Units/Actions/ActionView.cs
@@ -1,33 +1,43 @@
-using System;
+using Battles;
 using Targeting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Units
 {
     [RequireComponent(typeof(Button))]
-    public class ActionView : MonoBehaviour, IPoolable
+    public class ActionView : MonoBehaviour, IPoolable, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] private Image image;
+        [SerializeField] private Image selectedBackground;
         private Button _button;
         private IUnit _unit;
         private IAction _action;
         private UnitService _unitService;
+        private SelectService _selectService;
+        private bool _isSelected;
         
         private void Awake()
         {
             _button = GetComponent<Button>();
             _unitService = Locator.Get<UnitService>();
+            _selectService = Locator.Get<SelectService>();
         }
 
         private void OnEnable()
         {
             _button.onClick.AddListener(OnSelect);
+            _selectService.OnActiveContextChanged += OnActiveContextChanged;
         }
         
         private void OnDisable()
         {
             _button.onClick.RemoveListener(OnSelect);
+            if (_selectService != null)
+            {
+                _selectService.OnActiveContextChanged -= OnActiveContextChanged;
+            }
         }
 
         public void Bind(IUnit unit, IAction action)
@@ -67,7 +77,23 @@ namespace Units
             Unbind();
         }
 
-        public void OnSelect()
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (!_isSelected)
+            {
+                selectedBackground.gameObject.SetActive(true);
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (!_isSelected)
+            {
+                selectedBackground.gameObject.SetActive(false);
+            }
+        }
+
+        private void OnSelect()
         {
             if (_unit.CurrentEnergy < _action.Config.EnergyCost)
             {
@@ -75,8 +101,29 @@ namespace Units
             }
             
             _unitService.Perform(_unit, _action);
+            UpdateSelected(true);
         }
+        
+        private void UpdateSelected(bool isSelected)
+        {
+            selectedBackground.gameObject.SetActive(isSelected);
+            _isSelected = isSelected;
+        }
+        
+        private void OnActiveContextChanged(TeamType team, ISelectContext context)
+        {
+            if (team != TeamType.Player)
+            {
+                return;
+            }
 
+            if (_isSelected)
+            {
+                UpdateSelected(false);
+            }
+        }
+        
         public GameObject Prefab { get; set; }
+       
     }
 }
